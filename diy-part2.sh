f35148c7eb4ad55833d18d5c5dd20bf868efbf7c
missyea <615565545@qq.com>
2023-11-23 22:47:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,14 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.*addr_offset.*1/10.10.$((addr_offset++)).254/g' package/base-files/files/bin/config_generate
+
+sed -i "/network.\$1.proto='static'/i\                                set network.\$1.gateway='10.10.10.10'" package/base-files/files/bin/config_generate
+
+sed -i "/timezone='UTC'/a\                set system.@system[-1].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
+sed -i "s/timezone='UTC'/timezone='CST-8'/g" package/base-files/files/bin/config_generate
+
+sed -i "/system.ntp.enable_server/a\                add_list system.ntp.server='ntp.aliyun.com'" package/base-files/files/bin/config_generate
+
+sed -i 's/root:/root:$1$nZL.pSU8$iINNlIkw.K6U5U6NH8BC31/' package/base-files/files/etc/shadow
